068ac663c57bd9e393b4c0f2bc606549f4044981
Alex Rossell Hayes <alexander@rossellhayes.com>
2020-09-12 15:27:43 -0700

    Initial commit

---
+++ R/play_2048.R
@@ -0,0 +1,76 @@
+#' Play 2048 in the console
+#'
+#' @param size A vector of one or two integers giving the grid's dimensions.
+#'   If length one, a square grid is generated.
+#'   Defaults to `4`, which generates a 4-by-4 grid.
+#'
+#' @return Generates an interactive game of 2048 in the console.
+#' @export
+
+play_2048 <- function(size = 4) {
+  if (!is.null(ongoing_game()) && ask_resume()) {
+    return(resume_2048())
+  }
+
+  if (
+    length(size) < 1 || length(size) > 2 ||
+    !is.numeric(size) || size != size %/% 1
+  ) {
+    stop("`size` must be a vector of one or two integers.", call. = FALSE)
+  } else if (any(size < 2)) {
+    stop("Each dimension of `size` must be at least 2.")
+  }
+
+  for (i in seq_len(size ^ 2 + size)) {
+    do.call(
+      crayon::make_style, c(as.list(bg[i]), list(bg = TRUE, colors = 256))
+    )
+    do.call(
+      crayon::make_style, c(as.list(fg[i]), list(grey = TRUE, colors = 256))
+    )
+  }
+
+  twenty48_env$twenty48_game <- Twenty48$new(size)
+}
+
+#' @rdname play_2048
+#' @export
+
+resume_2048 <- function() {
+  if (is.null(ongoing_game())) {
+    stop("No ongoing twenty48 game to resume.", call. = FALSE)
+  }
+
+  ongoing_game()$play()
+}
+
+twenty48_env <- new.env()
+
+ongoing_game <- function() {
+  twenty48_env$twenty48_game
+}
+
+ask_resume <- function() {
+  if (is.null(ongoing_game())) {return(FALSE)}
+
+  cat(
+    "There is an ongoing game of twenty48.",
+    "Do you want to resume? (y/n)"
+  )
+
+  response <- substr(readline("> "), 1, 1)
+
+  while (TRUE) {
+    switch(
+      response,
+      y = return(TRUE),
+      n = return(FALSE),
+      {response <- invalid_response()}
+    )
+  }
+}
+
+invalid_response <- function() {
+  cat('I didn\'t understand that input. Please type "y" or "n" or press ESC.')
+  substr(readline("> "), 1, 1)
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# twenty48 <img src="man/figures/logo.png?raw=TRUE" align="right" height="138" />
+
+<!-- badges: start -->
+
+[![](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+[![License:
+MIT](https://img.shields.io/badge/license-MIT-blueviolet.svg)](https://cran.r-project.org/web/licenses/MIT)
+
+<!-- badges: end -->
+
+A clone of [Gabriele
+Cirulli’s 2048](https://github.com/gabrielecirulli/2048) in R.
+
+## Installation
+
+<!-- You can install the released version of **twenty48** from [CRAN](https://cran.r-project.org/package=twenty48) with: -->
+
+<!-- ``` {r eval = FALSE} -->
+
+<!-- install.packages("twenty48") -->
+
+<!-- ``` -->
+
+You can install the development version of **twenty48** from
+[GitHub](https://github.com/rossellhayes/twenty48) with:
+
+``` r
+# install.packages("remotes")
+remotes::install_github("rossellhayes/twenty48")
+```
+
+## Example
+
+![Gameplay recording](man/figures/recording.gif)
+
+-----
+
+Hex sticker font is [Source Sans
+Pro](https://github.com/adobe-fonts/source-sans-pro) by
+[Adobe](https://www.adobe.com).
+
+Recording made with
+[ScreenToGif](https://github.com/NickeManarin/ScreenToGif/) by [Nicke
+Manarin](https://github.com/NickeManarin).
+
+Please note that **twenty48** is released with a [Contributor Code of
+Conduct](https://contributor-covenant.org/version/2/0/CODE_OF_CONDUCT.html).
+By contributing to this project, you agree to abide by its terms.
